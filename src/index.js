f76e1ca99d84e7123b7c33c5d6163de0cb8db901
Rinor Rama <rinor.rama95@gmail.com>
2021-11-23 14:59:59 +0100

    created first card element

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-// - Make sure you check and understand the data that is given to you!
-// - Create a card using JS that represents a single pokemon, use the example image as a reference. You will also find an HTML example commented out in the index.html
-// - Use the exact CSS classes you see in the example HTML to obtain the same style for each card
-// - The cards should be nested inside <ul class="cards"></ul>
-// - Use the official-artwork object key as the images for the card. The images are all inside of the sprites key, in each pokemon object
-// pokemon.sprites.other['official-artwork'].front_default
+const ulCardsEl = document.querySelector(".cards");
 
-// - Render all the cards on the page that represents all the pokemons, recreating the same layout, using JS
+const pokemon = window.data[0];
+console.log(pokemon);
 
-console.log(data);
+const liCardEl = document.createElement("li");
+liCardEl.setAttribute("class", "card");
+const h2CardTitleEl = document.createElement("h2");
+h2CardTitleEl.innerText = pokemon.name;
+h2CardTitleEl.setAttribute("class", "card--title");
+liCardEl.append(h2CardTitleEl);
+
+const imgCardEl = document.createElement("img");
+imgCardEl.setAttribute("width", "256");
+imgCardEl.setAttribute("class", "card--img");
+imgCardEl.src = pokemon.sprites.front_default;
+liCardEl.append(imgCardEl);
+
+const ulCardEl = document.createElement("ul");
+ulCardEl.setAttribute("class", "card--text");
+
+const statsArray = ["HP", "ATTACK", "DEFENSE", "SPECIAL-ATTACK", "SPECIAL-DEFENSE", "SPEED"];
+
+for (let i = 0; i < statsArray.length; i++) {
+	const statName = statsArray[i];
+	const statValue = pokemon.stats[i].base_stat;
+	const liEl = document.createElement("li");
+	liEl.innerText = `${statName}: ${statValue}`;
+	ulCardEl.append(liEl);
+}
+
+liCardEl.append(ulCardEl);
+
+ulCardsEl.append(liCardEl);
